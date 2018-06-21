61d055043d3c1fbfefc300f69edc506e37ba6792
Marisol Smith <marisolsmith@gmail.com>
2018-06-20 21:16:32 -0400

    first commit

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,19 @@
+class ProductsController < ApplicationController
+
+def show
+  @product = Product.new
+end
+
+  def create
+  @product = Product.new(params[:product])
+  @product.save
+  redirect_to category_path(:id)
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :price)
+  end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  resources :categories
+    resources :products, only: [:create]
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'categories#index', as: 'index'
+end
